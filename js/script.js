e97231a5de6921d93db107ebb5954f7b02de7256
danielld75 <danielld@poczta.fm>
2018-03-04 08:06:38 +0100

    add navigation left right

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
-$(function(){
+$(function () {
+  var MARGIN = 400;
+  var ANIMATION_DURATION = 1500;
+
   var carouselList = $("#carousel ul");
-  var moveFirstSlide = function(){
+  var moveFirstSlide = function () {
     var firstItem = carouselList.find("li:first");
     var lastItem = carouselList.find("li:last");
     lastItem.after(firstItem);
     carouselList.css({marginLeft: 0});
   };
-  var changeSlides = function(){
-    carouselList.animate({'marginLeft':-400}, 1000, moveFirstSlide);
+
+  var changeSlides = function () {
+    carouselList.animate({'marginLeft': -MARGIN}, ANIMATION_DURATION, moveFirstSlide);
   };
+
   setInterval(changeSlides, 3000);
 
+  function moveLastSlide() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.before(lastItem);
+    carouselList.css({marginLeft: -MARGIN});
+  }
+
+  function changesSlidesBack() {
+    moveLastSlide();
+    carouselList.animate({'marginLeft': 0}, ANIMATION_DURATION);
+  }
+
+  var rightRow = $('#rightRow');
+  rightRow.on('click', changesSlidesBack);
+
   var leftRow = $('#leftRow');
-  var left = function() {
-    leftRow.click();
-  };
-  left();
+  leftRow.on('click', changeSlides);
 });
